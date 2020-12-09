82d35d746dfe2ac801e0e03338f12b4a18fa5745
Fluff <dune@fluffigt.net>
2020-12-09 15:06:40 +0100

    2020 - day 09

---
+++ src/se/fluff/aoc2020/days/Day09.java
@@ -0,0 +1,97 @@
+package se.fluff.aoc2020.days;
+
+import se.fluff.aoc.AocDay;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * Created by Fluff on 2020-12-09.
+ */
+public class Day09 extends AocDay {
+
+    public Day09() {
+
+    }
+
+    @Override
+    public String a(Scanner in) {
+        ArrayList<Long> numbers = new ArrayList<>();
+        while(in.hasNext())
+            numbers.add(Long.parseLong(in.nextLine()));
+
+        ArrayList<Long> preamble = new ArrayList<>();
+        for(long n : numbers) {
+            if(preamble.size() == 25) {
+                boolean found = false;
+                HashSet<Long> s = new HashSet<>();
+                for (Long aLong : preamble) {
+                    if (s.contains(n - aLong)) {
+                        found = true;
+                        break;
+                    }
+                    s.add(aLong);
+                }
+                if(!found)
+                    return String.valueOf(n);
+                preamble.remove(0);
+            }
+            preamble.add(n);
+        }
+        return null;
+    }
+
+    @Override
+    public String b(Scanner in) {
+        long weak = -1;
+        ArrayList<Long> numbers = new ArrayList<>();
+        while(in.hasNext())
+            numbers.add(Long.parseLong(in.nextLine()));
+
+        ArrayList<Long> preamble = new ArrayList<>();
+        for(long n : numbers) {
+            if(preamble.size() == 25) {
+                boolean found = false;
+                HashSet<Long> s = new HashSet<>();
+                for (Long aLong : preamble) {
+                    if (s.contains(n - aLong)) {
+                        found = true;
+                        break;
+                    }
+                    s.add(aLong);
+                }
+                if(!found) {
+                    weak = n;
+                    break;
+                }
+                preamble.remove(0);
+            }
+            preamble.add(n);
+        }
+
+        ArrayList<Long> contiguous = new ArrayList<>();
+        int i = 0;
+        while(contiguous.stream().reduce(0L, Long::sum) != weak) {
+            contiguous = new ArrayList<>();
+            int j = i;
+
+            while(contiguous.stream().reduce(0L, Long::sum) < weak)
+                contiguous.add(numbers.get(j++));
+
+            i++;
+        }
+
+        return String.valueOf(
+                contiguous
+                        .stream()
+                        .min(Long::compare)
+                        .get() +
+                contiguous
+                        .stream()
+                        .max(Long::compare)
+                        .get()
+        );
+    }
+}
